e964b6ad509410d96821e1a59f5ae83eb8bb6447
jamilabarasa <jamilabarasa18@gmail.com>
2024-03-27 22:29:04 +0300

    Added resolveString method

--- src/main/java/service/USSDService.java
+++ src/main/java/service/USSDService.java
@@ -35,21 +35,112 @@ public class USSDService {
     }
 
     public List<USSDNode> loadChildrenByParent(int parentId) {
+
+//        load children if it is not terminal,and perform the necessary action if it is
+//        terminal
+        if (isNodeTerminal(parentId)) {
+
+            //call method to perform the action
+            return null;
+        }
+
         return ussdNodes.stream()
                 .filter(node -> node.getParent() == parentId)
                 .collect(Collectors.toList());
     }
 
 
-    public List<USSDNode> resolveUssdString(String ussdString) {
 
-        return null;
+
+    public List<USSDNode> resolveUssdString(String ussdString) {
+        // Remove the first five characters (USSD code,*544*) including the first start and the last character (#)
+        ussdString = ussdString.substring(5, ussdString.length() - 1);
+
+        // Split the string by asterisk (*) and store the result in an array
+        String[] nodes = ussdString.split("\\*");
+
+        int currentnode = Integer.parseInt(nodes[0]);
+
+
+        // Check if the first node is a valid parent node
+        if (nodes.length > 0) {
+            try {
+                int nodeId = Integer.parseInt(nodes[0]);
+                boolean first = true;
+
+
+                if (findNode(nodeId, first)) {
+                    first = false;
+
+
+                    // Loop through each node in the array
+                    // Find the node in ussdNodes list by its ID
+                    // If currentNode is null, it means this is the first node in the sequence
+                    // Check if it's a root node
+                    // If it's a root node, set it as currentNode
+                    // If it's not a root node, return all root nodes
+                    // Check if the current node is a child of the previous node
+                    // If it's a child node, set it as currentNode and continue to the next node
+                    // If it's not a child node, return all children of the previous node
+                    // if node is terminal true ,call the action function
+
+                    return loadChildrenByParent(nodeId);
+
+                } else {
+
+                    if (first) {
+                        // If the first node is not a valid parent node, return all root nodes
+                        return loadRootNodes();
+                    } else {
+                        // If the first node is a valid parent node, return its children
+
+                        return loadChildrenByParent(nodeId);
+                    }
+
+                }
+
+            } catch (NumberFormatException e) {
+                // If the first element is not a valid number, return all root nodes
+                return loadRootNodes();
+            }
+        } else {
+            // If there are no nodes, return all root nodes
+            return loadRootNodes();
+        }
     }
 
-    
+    //function to find the node
+    public boolean findNode(int nodeId, boolean first) {
+
+        if (!first) {
+            // Find the node in ussdNodes list by its ID
+            Optional<USSDNode> optionalNode = ussdNodes.stream()
+                    .filter(node -> node.getId() == nodeId)
+                    .findFirst();
+
+            return optionalNode.isPresent();
+        }
+
+        // Find the node in ussdNodes list by its ID
+        Optional<USSDNode> optionalNode = ussdNodes.stream()
+                .filter(node -> node.getId() == nodeId && node.getParent() == -1)
+                .findFirst();
 
 
+        return optionalNode.isPresent();
+    }
 
+    //function to check if node is terminal
+    public boolean isNodeTerminal(int nodeId) {
+
+        // Find the node in ussdNodes list by its ID
+        Optional<USSDNode> optionalNode = ussdNodes.stream()
+                .filter(node -> node.getId() == nodeId && node.isTerminal() == true)
+                .findFirst();
+
+
+        return optionalNode.isPresent();
+    }
 
 
 }
